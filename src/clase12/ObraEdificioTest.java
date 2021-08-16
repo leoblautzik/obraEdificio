4f697389579bef9d4ce5d343431ffdf9fd16d633
Leonardo Blautzik <leoblautzik@gmail.com>
2021-08-16 20:31:02 -0300

    Agrego los test

---
+++ src/clase12/ObraEdificioTest.java
@@ -0,0 +1,14 @@
+package clase12;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ObraEdificioTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
